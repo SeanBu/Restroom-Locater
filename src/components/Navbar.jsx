252206f4a430ae0125bf5f91cb153b06aa22adcf
SeanBu <seanbuchas@gmail.com>
2022-10-12 15:07:30 -0400

    add basic styling to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
-        <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/register">Register</Link></li>
+        <div className='navContainer'>
+            <ul className='navList'>
+                <li className='navItem'><Link className='navLink' to="/">Home</Link></li>
+                <li className='=navItem'><Link className='navLink' to="/login">Login</Link></li>
+                <li className='navItem'><Link className='navLink' to="/register">Register</Link></li>
             </ul>
         </div>
     )
