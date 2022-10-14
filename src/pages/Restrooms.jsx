af1fc5c8bd4e5bf5fe808acb7040412b08749b75
SeanBu <seanbuchas@gmail.com>
2022-10-14 17:22:32 -0400

    finish building restroom and submissions pages

--- src/pages/Restrooms.jsx
+++ src/pages/Restrooms.jsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from 'react';
+
 function Restrooms() {
+
+    const [restrooms, setRestrooms] = useState([]);
+
+    async function fetchData() {
+        const response = await fetch("/admin/allrestrooms", { headers: { "Content-type": "application/json" } });
+        const jResponse = await response.json();
+        setRestrooms(...jResponse);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
     return (
         <div>
-
+            {restrooms ? restrooms.map(restroom => {
+                return (
+                    <div>
+                        <p>Restroom Id: {restroom._id}</p>
+                        <p>Lat: {restroom.lat}</p>
+                        <p>Lng Email: {restroom.lng}</p>
+                        <p>Address: {restroom.address}</p>
+                        <p>Cleanliness Rating: {restroom.cleanlinessRating}</p>
+                        <p>Location Rating: {restroom.locationRating}</p>
+                        <p>Description: {restroom.description}</p>
+                        <p>Image:</p>
+                        <img src={restroom.image} alt="Restroom Image"></img>
+                        <br></br>
+                    </div>
+                )
+            }) : console.log("Loading Users...")}
         </div>
     )
 }
--- src/pages/Submissions.jsx
+++ src/pages/Submissions.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from 'react';
+
 function Submissions() {
+    const [submissions, setSubmissions] = useState([]);
+
+    async function fetchData() {
+        const response = await fetch("/admin/allsubmissions", { headers: { "Content-type": "application/json" } });
+        const jResponse = await response.json();
+        setSubmissions(...jResponse);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
     return (
         <div>
-
+            {submissions ? submissions.map(submission => {
+                return (
+                    <div>
+                        <p>Submission Id: {submission._id}</p>
+                        <p>User Id: {submission.user}</p>
+                        <p>Restroom Id: {submission.restroom}</p>
+                        <br></br>
+                    </div>
+                )
+            }) : console.log("Loading Users...")}
         </div>
     )
 }
